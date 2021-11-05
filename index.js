fc6a8420bbcd955d55d657c2a3b97525afc7357c
Muhammad Hamza <hamxa.ch@outlook.com>
2021-11-05 10:59:53 +0500

    index file

---
+++ index.js
@@ -0,0 +1,10 @@
+console.time('time took')
+console.log("This Muhammad Hamza")
+console.log(23*4);
+console.log([12,23,45,'hamza']);
+//console.log({hamza:'programmer', language:"Urdu", age:21})
+//console.table({hamza:'programmer', language:"Urdu", age:21})
+
+//console.warn("this is not poosible")
+console.assert(67>119, "This is age is not possible");
+console.timeEnd('time took')
